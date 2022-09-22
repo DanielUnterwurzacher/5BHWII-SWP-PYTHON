bae8ee79ca6e6c20d23b8ce9957d09a675727390
uwux0 <87463507+uwux0@users.noreply.github.com>
2022-09-22 20:19:24 +0200

    Update Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import random
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def generateArray():
+    global numbers
+    numbers = []
+    for i in range(1,46):
+        numbers.append(i)
 
+def drawNumber():
+    help1=44
+    help2=1
+    drawnNumbersArray = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    for i in range(6):
+        indexOfNumber = random.randint(0,help1)
+        help1=help1-1
+        randomNumber = numbers[indexOfNumber]
+        numbers[indexOfNumber] = numbers[len(numbers)-help2]
+        numbers[len(numbers)-help2] = randomNumber
+        drawnNumbersArray.append(numbers[len(numbers) - help2])
+        help2 = help2 + 1
+    return drawnNumbersArray
 
+def generateDict():
+    dict = {}
+    for i in range(1,46):
+        dict[i] = 0
+    return dict
 
-# Press the green button in the gutter to run the script.
+def statistic():
+    dictNumbers = generateDict()
+
+    for i in range(1000):
+        numbers = drawNumber()
+        for i in numbers:
+            dictNumbers[i] = dictNumbers[i] +1
+
+    return dictNumbers
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    generateArray()
+    print(drawNumber())
+    print(statistic())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
